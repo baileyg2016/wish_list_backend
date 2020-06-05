18045d0c82e2eb39a7f5ebc722ed33d6199add27
baileyg2016 <bspell20@vt.edu>
2020-06-04 22:51:49 -0400

    trying to get https working

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const express = require('express');
 const dotenv = require('dotenv');
+const express = require('express');
+const forceSSL = require('express-force-ssl');
+const fs = require('fs');
+var http = require('http');
+const https = require('https');
 const util = require('util');
 const { Client } =  require('pg');
-
-// const pool = require('./db');
 
 dotenv.config();
 
@@ -16,25 +19,66 @@ pgClient.connect();
 
 const app = express();
 const port = process.env.PORT || 5000;
+var key = fs.readFileSync('private.key');
+// var cert = fs.readFileSync('primary.crt' );
+var ca = fs.readFileSync('mydomain.csr' );
+var serverOptions = {
+    key: key,
+    // cert: cert,
+    ca: ca
+};
 
 app.use(bodyParser.json());
+app.use(forceSSL); // for http to go through https (:
 
+// perform login credentials
 app.get("/", async (req, res, next) => {
-    await pgClient.query("SELECT * FROM \"Users\"", (err, result) => {
+    // should check for duplicates
+
+    await pgClient.query("SELECT * FROM users", (err, result) => {
         if (err) {
             console.log(err);
             res.status(400).send(err)
         }
         res.send(result.rows)
         prettyPrintResponse(result.rows)
+    });
+});
+
+app.post("/", async (req, res) => {
+    // bcrypt goes here
+    await pgClient.query(`SELECT adduser('${req.body.firstName}'::text, '${req.bodylastName}'::text, '${req.body.email}'::text, 'testing123'::text);`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err)
+        }
+        res.send(result.rows)
+        prettyPrintResponse(result.rows)
+    });
+});
+
+app.post("/addItem", async (req, res) => {
+    await pgClient.query(`SELECT addItem(\'${req.body.user}\'::text, \'${req.body.name}\'::text, \'${req.body.url}\'::text, ${req.body.cost}::money, \'${req.body.size}\'::text, \'${req.body.custom}\'::text);`, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(err);
+            return;
+        }
+
+        res.send(result)
+        prettyPrintResponse(result)
     })
-})
+});
 
 
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
 
+http.createServer(app).listen(80);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+https.createServer(serverOptions, app).listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
 
 var prettyPrintResponse = response => {
